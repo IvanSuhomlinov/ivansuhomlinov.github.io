9e89a1a1ebab79272f04171e2f2c3e6bf1fb3cc6
Ivan Suhomlinov <isuhomlinov57@gmail.com>
2025-01-16 18:12:40 +0500

    Start of the project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-
-
-
-function Main() {
-  return (
-    <div className='main-div'>
-        <div className='gym-place'>Папилон</div>
-        <div className='main-text-booking'>Забронировать время на спортзал</div>
-        <div className='main-div-booking-button'>
-          <a href='booking.html' className='main-booking-button'>Забронировать</a>
-        </div>
-    </div>
-    
-    
-  )
-}
-
-//npm run start для запуска проекта 
+import React from "react";
+//npm  start для запуска проекта 
 function App() {
 
 
   return (
 
     <div>
-      <Main />
+      
       
     </div>
   );
