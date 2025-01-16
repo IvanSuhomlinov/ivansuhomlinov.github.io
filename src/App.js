c10e5538a152b4c5ab582eaee8f416af5f8d49a4
Ivan Suhomlinov <isuhomlinov57@gmail.com>
2025-01-16 15:07:51 +0500

    change components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import Header from "./Components/Header";
-import Main from "./Components/Main";
+
+
+
+function Main() {
+  return (
+    <div className='main-div'>
+        <div className='gym-place'>Папилон</div>
+        <div className='main-text-booking'>Забронировать время на спортзал</div>
+        <div className='main-div-booking-button'>
+          <a href='booking.html' className='main-booking-button'>Забронировать</a>
+        </div>
+    </div>
+    
+    
+  )
+}
 
 //npm run start для запуска проекта 
 function App() {
+
+
   return (
+
     <div>
-      <Header />
       <Main />
+      
     </div>
   );
 }
