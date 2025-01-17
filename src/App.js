d755b11052d5f34bd2b9d2e7f556a6f35bb39b9d
Ivan Suhomlinov <isuhomlinov57@gmail.com>
2025-01-17 17:33:09 +0500

    DatePicker 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import * as React from 'react';
-import BasicDatePicker from './Components/DatePicker';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import dayjs from 'dayjs';
 
-function App(){
+function BasicDatePicker() {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DemoContainer components={['DatePicker']}>
+        <DatePicker label="Введите дату" format='DD.MM.YYYY'  defaultValue={dayjs(Date.now())} />
+      </DemoContainer>
+      
+    </LocalizationProvider>
+
+    
+  );
+}
+
+
+const App = () =>{
 
 
   return (
